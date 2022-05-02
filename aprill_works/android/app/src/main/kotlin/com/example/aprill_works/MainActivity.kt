62641cfff6c7eea5a242a14cf1f9b086d5ddf249
gorkemarslanbogan <mavisimsek940@gmail.com>
2022-05-03 01:35:02 +0300

    Flutter Login Page
    My First Project İn Github
    Flutter Login Screen

---
+++ aprill_works/android/app/src/main/kotlin/com/example/aprill_works/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.aprill_works
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ aprill_works/lib/main.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() =>
+    runApp(MaterialApp(debugShowCheckedModeBanner: false, home: MyApp()));
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Her Güne Örnek',
+      theme: ThemeData.light().copyWith(
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.amber,
+        ),
+        iconTheme: new IconThemeData(
+          color: Colors.blue,
+          size: 30,
+        ),
+      ),
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(244, 255, 255, 255),
+        appBar: PreferredSize(
+            preferredSize: const Size.fromHeight(250),
+            child: Stack(
+              children: [
+                AppBar(
+                  systemOverlayStyle: SystemUiOverlayStyle.light,
+                  leading: IconButton(
+                    icon: Icon(
+                      Icons.navigate_before,
+                      color: Colors.black,
+                    ),
+                    onPressed: () {},
+                  ),
+                  centerTitle: true,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.vertical(
+                          bottom: Radius.elliptical(
+                              MediaQuery.of(context).size.width, 250))),
+                ),
+                Image.asset(
+                  "assets/images/people.png",
+                  height: 250,
+                  width: MediaQuery.of(context).size.width,
+                )
+              ],
+            )),
+        body: HomeScreen(),
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Wrap(
+          alignment: WrapAlignment.center,
+          children: [
+            textwidgetValue(
+              textValue: "What sould we call you?",
+              textwidgetStyle: TextStyle(color: Colors.black, fontSize: 25),
+            ),
+            textwidgetValue(
+              textValue: "My name is Goldburg, and yours?",
+              textwidgetStyle: TextStyle(color: Colors.grey, fontSize: 18),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Wrap(
+                alignment: WrapAlignment.center,
+                runSpacing: 25,
+                children: [
+                  _textFieldWidget(textfieldValue: "First Name"),
+                  _textFieldWidget(textfieldValue: "Last Name"),
+                ],
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width - 50,
+              height: 55,
+              child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text("Next"),
+                  style: OutlinedButton.styleFrom(
+                    backgroundColor: Color.fromARGB(255, 156, 68, 9),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.vertical(
+                          bottom: Radius.elliptical(15, 15),
+                          top: Radius.elliptical(15, 15)),
+                    ),
+                  )),
+            ),
+          ],
+        ),
+      ],
+    );
+    
+  }
+}
+
+class textwidgetValue extends StatelessWidget {
+  const textwidgetValue(
+      {Key? key, required this.textValue, required this.textwidgetStyle})
+      : super(key: key);
+  final String textValue;
+  final TextStyle textwidgetStyle;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Text(
+        textValue,
+        style: textwidgetStyle,
+      ),
+    );
+  }
+}
+
+class _textFieldWidget extends StatelessWidget {
+  const _textFieldWidget({Key? key, required this.textfieldValue})
+      : super(key: key);
+  final String textfieldValue;
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+        decoration: InputDecoration(
+      fillColor: Colors.white,
+      filled: true,
+      hintText: textfieldValue,
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.vertical(
+            bottom: Radius.elliptical(15, 15), top: Radius.elliptical(15, 15)),
+      ),
+    ));
+  }
+}
